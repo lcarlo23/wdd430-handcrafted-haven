b7d524e0eb2bcb021cda579b74abef99d94e1dda
bostler3 <bostler3@gmail.com>
2026-05-30 11:41:07 -0600

    Add footer component and integrate into layout.tsx. Also moved NavBar component from page.tsx to layout.tsx

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import NavLinks from "@/components/navlinks";
+
+export default function Footer() {
+  const current_year = new Date().getFullYear();
+    return (
+    <div className="footer">
+      <div className="footer-navlinks-container">
+        <NavLinks />
+      </div>
+      {/* <br></br> */}
+      <p className="footer-copyright-text">&copy; {current_year}, Team01-WDD430 (Block 3, 2026)  </p>
+    </div>
+  );
+}
