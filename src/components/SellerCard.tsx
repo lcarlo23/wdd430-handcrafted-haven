73a17fc9c2773aa76dfd4f361d22699105e5148d
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-06-10 17:19:09 +0200

    Create seller card component

---
+++ src/components/SellerCard.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+interface Seller {
+  id: string;
+  profile_image: string;
+  name: string;
+}
+
+interface SellerCardProps {
+  seller: Seller;
+}
+
+export default function SellerCard({ seller }: SellerCardProps) {
+  return (
+    <Link href={`/seller/${seller.id}`} className="seller-card">
+      <Image src={seller.profile_image} alt={seller.name} className="card-image" />
+      <div className="seller-details">
+        <h3 className="seller-name">{seller.name}</h3>
+      </div>
+    </Link>
+  );
+}
