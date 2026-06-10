928960cdb854ceddb8756305e194409b754daca3
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-06-10 17:24:17 +0200

    Fix image element

--- src/components/SellerCard.tsx
+++ src/components/SellerCard.tsx
@@ -14,7 +14,13 @@ interface SellerCardProps {
 export default function SellerCard({ seller }: SellerCardProps) {
   return (
     <Link href={`/seller/${seller.id}`} className="seller-card">
-      <Image src={seller.profile_image} alt={seller.name} className="card-image" />
+      <Image
+        src={seller.profile_image}
+        alt={seller.name}
+        width={200}
+        height={200}
+        className="card-image"
+      />
       <div className="seller-details">
         <h3 className="seller-name">{seller.name}</h3>
       </div>
