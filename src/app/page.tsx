7365f00d3b03d051ff0f24616eb8ad9f8e585cc3
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-06-11 17:09:18 +0200

    Clean css code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.css';
+import './home.css';
 import ProductRow from '@/components/ProductRow';
 import FeaturedHandcrafter from '@/components/FeaturedHandcrafter';
 import ProductGrid from '@/components/ProductGrid';
@@ -20,8 +20,8 @@ export default async function Home({
     typeof resolvedParams?.maxPrice === 'string' ? resolvedParams.maxPrice : undefined;
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="home-page">
+      <main className="home-main">
         <ProductRow
           rowId="77777777-7777-7777-7777-777777777777"
           sectionTitle="Top Deals"
